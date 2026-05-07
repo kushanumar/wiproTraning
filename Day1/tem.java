96eec9c3357836057534fed543f68e2db3b9f9f2
kushanumar <shanugn989@gmail.com>
2026-05-07 17:49:42 +0530

    Delete Day1 directory

--- Day1/tem.java
+++
@@ -1,9 +0,0 @@
-package Day1;
-
-public class tem {
-	 public static void main(String[] args) {	        
-	        float celsius = 25.0f;	   
-	        float fahrenheit = (celsius * 9.0f / 5.0f) + 32;	       
-	        System.out.println(celsius + "°C is equal to " + fahrenheit + "°F");
-	    }
-}
